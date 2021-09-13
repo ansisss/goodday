86cf5e791010cfc3d374bdc1ff0ea7af3ee5246f
ansisss <67478763+ansisss@users.noreply.github.com>
2021-09-14 00:02:13 +0300

    Update create.blade.php

--- example-app/resources/views/posts/create.blade.php
+++ example-app/resources/views/posts/create.blade.php
@@ -1,34 +1,34 @@
 @extends('layouts.app')
 @section('content')
-    <h1>Create post</h1>
+    <h1>Izveidot reklāmu</h1>
     {!! Form::open(['action' => 'App\Http\Controllers\PostsController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data',  'class' => 'form']) !!}
     <div class="row">
         <div class="col-lg-6 col-md-2">
             <div class="form-group">
-                {{Form::label('title', 'Title')}}
+                {{Form::label('title', 'Nosaukums')}}
                 {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
             </div>
              <div class="form-group">
-                 {{Form::label('first_credit', 'First_credit')}}
+                 {{Form::label('first_credit', 'Pirmā kredīta summa')}}
                  {{Form::text('first_credit', '', ['class' => 'form-control', 'placeholder' => 'Firsts_credit'])}}
              </div>
 
             <div class="form-group">
-                {{Form::label('sum', 'Sum')}}
+                {{Form::label('sum', 'Kredīta summa')}}
                 {{Form::text('sum', '', ['class' => 'form-control', 'placeholder' => 'Sum'])}}
             </div>
         </div>
             <div class="col-lg-6 col-md-2">
             <div class="form-group">
-                {{Form::label('age', 'Age')}}
+                {{Form::label('age', 'Vecums')}}
                 {{Form::text('age', '', ['class' => 'form-control', 'placeholder' => 'Age'])}}
             </div>
                 <div class="form-group">
-                {{Form::label('term', 'Term')}}
+                {{Form::label('term', 'Termiņš')}}
                 {{Form::text('term', '', ['class' => 'form-control', 'placeholder' => 'Term'])}}
             </div>
                 <div class="form-group">
-                    {{Form::label('extra', 'Extra')}}
+                    {{Form::label('extra', 'Papildus atlaides')}}
                     {{Form::text('extra', '', ['class' => 'form-control', 'placeholder' => 'Extra'])}}
                 </div>
             </div>
@@ -50,13 +50,13 @@
     </div>
 
             <div class="form-group">
-                {{Form::label('body', 'Body')}}
+                {{Form::label('body', 'Kredīta apraksts')}}
                 {{Form::textarea('body', '', ['id' => 'article-ckeditor','class' => 'form-control', 'placeholder' => 'Body text'])}}
             </div>
             <div class="'form-group">
                 {{Form::file('cover_image')}}
             </div>
     <br>
-        {{Form::submit('Submit',['class' => 'btn btn-primary'])}}
+        {{Form::submit('Iesniegt',['class' => 'btn btn-primary'])}}
     {!! Form::close() !!}
 @endsection
