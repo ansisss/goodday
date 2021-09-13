e7ad175b5201778d7dc81c5d38395b4381f11a41
ansisss <67478763+ansisss@users.noreply.github.com>
2021-09-14 00:02:11 +0300

    Update navbar.blade.php

--- example-app/resources/views/inc/navbar.blade.php
+++ example-app/resources/views/inc/navbar.blade.php
@@ -30,8 +30,8 @@
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
                 @if (Auth::guest())
-                    <li><a href="{{ route('login') }}">Login</a></li>
-                    <li><a href="{{ route('register') }}">Register</a></li>
+                    <li><a href="{{ route('login') }}">Ieiet</a></li>
+                    <li><a href="{{ route('register') }}">Reģistrēties</a></li>
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -39,7 +39,7 @@
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
-                            <li><a href="/goodday/example-app/public/dashboard">Dashboard</a></li>
+                            <li><a href="/dashboard">Dashboard</a></li>
                             <li>
                                 <a href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
